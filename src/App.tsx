db2a1e7829a017ee316d39a54760c46ffed39360
Arnold <arnoldemmanuel15@gmail.com>
2022-02-14 11:21:33 +0100

    Moved new navbar to navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,9 @@ import './App.css';
 import { PostsContext } from './contexts/posts/PostsState';
 import { ActionTypes } from './contexts/types';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from './components/layout/Navbar';
+import NavbarComponent from './components/layout/Navbar';
 import Home from './components/pages/Home';
 import Footer from './components/pages/FooterSection';
-import NewNavBar from './components/layout/NewNavBar';
 
 const App: FC = () => {
   // The reason posts state is not used here is beacuse we bring in the state and dispatch
@@ -35,7 +34,7 @@ const App: FC = () => {
   return (
     <Router>
       <Fragment>
-        <NewNavBar />
+        <NavbarComponent />
         <Routes>
           <Route index element={<Home />} />
         </Routes>
--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,55 +1,29 @@
-import { Fragment, FC } from 'react';
+import { FC } from 'react';
+import { Navbar, Nav, Container } from 'react-bootstrap';
 
-const Navbar: FC = () => {
+const NavbarComponent: FC = () => {
   return (
-    <Fragment>
-      <nav
-        className="navbar px-md-0 navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light"
-        id="ftco-navbar"
-      >
-        <div className="container">
-          <a className="navbar-brand text-dark" href="index-2.html">
-            Read<i>it</i>.
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#ftco-nav"
-            aria-controls="ftco-nav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="oi oi-menu" /> Menu
-          </button>
-          <div className="collapse navbar-collapse" id="ftco-nav">
-            <ul className="navbar-nav ml-auto">
-              <li className="nav-item active">
-                <a href="index-2.html" className="nav-link">
-                  Home
-                </a>
-              </li>
-              <li className="nav-item">
-                <a href="blog.html" className="nav-link">
-                  Articles
-                </a>
-              </li>
-              <li className="nav-item">
-                <a href="about.html" className="nav-link">
-                  Team
-                </a>
-              </li>
-              <li className="nav-item">
-                <a href="contact.html" className="nav-link">
-                  Contact
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-    </Fragment>
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" fixed="top">
+      <Container>
+        <Navbar.Brand href="#home">
+          Read<span className="text-warning">it</span>{' '}
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="ml-auto">
+            <Nav.Link href="#features">Home</Nav.Link>
+            <Nav.Link href="#pricing">Articles</Nav.Link>
+          </Nav>
+          <Nav className="ml-auto">
+            <Nav.Link href="#deets">Team</Nav.Link>
+            <Nav.Link eventKey={2} href="#memes">
+              Contact
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 
-export default Navbar;
+export default NavbarComponent;
