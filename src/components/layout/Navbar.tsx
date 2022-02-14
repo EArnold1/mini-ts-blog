930aeab4c7c62f5dfa3a264f5f941b916d3bff3f
Arnold <arnoldemmanuel15@gmail.com>
2022-02-14 12:25:24 +0100

    Fixed home page header

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -3,20 +3,33 @@ import { Navbar, Nav, Container } from 'react-bootstrap';
 
 const NavbarComponent: FC = () => {
   return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" fixed="top">
+    <Navbar
+      collapseOnSelect
+      expand="lg"
+      bg="secondary"
+      variant="dark"
+      fixed="top"
+      className="navbar"
+    >
       <Container>
-        <Navbar.Brand href="#home">
+        <Navbar.Brand href="#home" className="">
           Read<span className="text-warning">it</span>{' '}
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="ml-auto">
-            <Nav.Link href="#features">Home</Nav.Link>
-            <Nav.Link href="#pricing">Articles</Nav.Link>
+            <Nav.Link href="#features" className="">
+              Home
+            </Nav.Link>
+            <Nav.Link href="#pricing" className="">
+              Articles
+            </Nav.Link>
           </Nav>
           <Nav className="ml-auto">
-            <Nav.Link href="#deets">Team</Nav.Link>
-            <Nav.Link eventKey={2} href="#memes">
+            <Nav.Link href="#deets" className="">
+              Team
+            </Nav.Link>
+            <Nav.Link href="#memes" className="">
               Contact
             </Nav.Link>
           </Nav>
--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -5,46 +5,22 @@ import Section1 from './Section1';
 const Home: FC = () => {
   return (
     <Fragment>
-      <div
-        className="hero-wrap"
-        style={{
-          backgroundImage: 'url(https://bit.ly/3JmAFKS)',
-        }}
-        data-stellar-background-ratio="0.5"
-      >
-        {/* <div className="overlay"></div> */}
-        <div className="container">
-          <div
-            className="row no-gutters slider-text js-fullheight align-items-center justify-content-start"
-            data-scrollax-parent="true"
-          >
-            <div className="col-md-12 ftco-animate ss">
-              <h2 className="subheading">Hello! Welcome to</h2>
-              <h1 className="mb-md-0 mb-4">Readit blog</h1>
-              <div className="row">
-                <div className="col-md-7">
-                  <div className="text">
-                    <p>
-                      Far far away, behind the word mountains, far from the
-                      countries Vokalia and Consonantia, there live the blind
-                      texts. Separated they live in Bookmarksgrove right at the
-                      coast of the Semantics, a large language ocean.
-                    </p>
-                    <div className="mouse">
-                      <a href="#" className="mouse-icon">
-                        <div className="mouse-wheel">
-                          <span className="ion-ios-arrow-round-down" />
-                        </div>
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
+      <div className="home text-secondary rounded p-5">
+        <div className="overlay" />
+        <section className="row mt-5">
+          <div className="col-sm-11">
+            <h4>Hi, Welcome to</h4>
+            <p className="display-1">Readit Blog</p>
+            <br />
+            <p className="lead">
+              Far far away, behind the word mountains, far from the countries
+              Vokalia and Consonantia, there live the blind texts. Separated
+              they live in Bookmarksgrove right at the coast of the Semantics, a
+              large language ocean.
+            </p>
           </div>
-        </div>
+        </section>
       </div>
-
       <Section1 />
     </Fragment>
   );
