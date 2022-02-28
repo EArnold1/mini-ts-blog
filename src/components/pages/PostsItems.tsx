c526a0640f8a61a5d5547a3baba7d23808c26212
Arnold <arnoldemmanuel15@gmail.com>
2022-02-28 10:53:50 +0100

    Added loading to illustration component

--- src/components/pages/PostsItems.tsx
+++ src/components/pages/PostsItems.tsx
@@ -28,74 +28,75 @@ const PostsItems: FC<PostsItemsModel> = (props) => {
 
   return (
     <Fragment>
-      {posts.map((post) => (
-        <div className="case" key={post.title}>
-          <div className="row">
-            <div className="col-md-6 col-lg-6 col-xl-8 d-flex">
-              <Link
-                to="news"
-                state={{
-                  content: post.content,
-                  img: post.urlToImage,
-                  title: post.title,
-                  desc: post.description,
-                  author: post.author,
-                  loading,
-                }}
-                className="img w-100 mb-md-0 mb-3"
-                style={{
-                  backgroundImage: `url(${post.urlToImage})`,
-                }}
-              />
-            </div>
-            <div className="col-md-6 col-lg-6 col-xl-4 d-flex">
-              <div className="text w-100 pl-md-3">
-                <span className="subheading">{post.source.name}</span>
-                <h2>
-                  <Link
-                    to="news"
-                    state={{
-                      content: post.content,
-                      img: post.urlToImage,
-                      title: post.title,
-                      desc: post.description,
-                      author: post.author,
-                      loading,
-                    }}
-                  >
-                    {post.title}
-                  </Link>
-                </h2>
-                <ul className="media-social list-unstyled">
-                  <li className="ftco-animate">
-                    <a href="#">
-                      <span className="icon-twitter" />
-                    </a>
-                  </li>
-                  <li className="ftco-animate">
-                    <a href="#">
-                      <span className="icon-facebook" />
-                    </a>
-                  </li>
-                  <li className="ftco-animate">
-                    <a href="#">
-                      <span className="icon-instagram" />
-                    </a>
-                  </li>
-                </ul>
-                <div className="meta">
-                  <p className="mb-0">
-                    <span>
-                      <Moment format="YYYY/MM/DD">{post.publishedAt}</Moment>
-                    </span>{' '}
-                    | <span>12 min read</span>
-                  </p>
+      {!loading &&
+        posts.map((post) => (
+          <div className="case" key={post.title}>
+            <div className="row">
+              <div className="col-md-6 col-lg-6 col-xl-8 d-flex">
+                <Link
+                  to="news"
+                  state={{
+                    content: post.content,
+                    img: post.urlToImage,
+                    title: post.title,
+                    desc: post.description,
+                    author: post.author,
+                    loading,
+                  }}
+                  className="img w-100 mb-md-0 mb-3"
+                  style={{
+                    backgroundImage: `url(${post.urlToImage})`,
+                  }}
+                />
+              </div>
+              <div className="col-md-6 col-lg-6 col-xl-4 d-flex">
+                <div className="text w-100 pl-md-3">
+                  <span className="subheading">{post.source.name}</span>
+                  <h2>
+                    <Link
+                      to="news"
+                      state={{
+                        content: post.content,
+                        img: post.urlToImage,
+                        title: post.title,
+                        desc: post.description,
+                        author: post.author,
+                        loading,
+                      }}
+                    >
+                      {post.title}
+                    </Link>
+                  </h2>
+                  <ul className="media-social list-unstyled">
+                    <li className="ftco-animate">
+                      <a href="#">
+                        <span className="icon-twitter" />
+                      </a>
+                    </li>
+                    <li className="ftco-animate">
+                      <a href="#">
+                        <span className="icon-facebook" />
+                      </a>
+                    </li>
+                    <li className="ftco-animate">
+                      <a href="#">
+                        <span className="icon-instagram" />
+                      </a>
+                    </li>
+                  </ul>
+                  <div className="meta">
+                    <p className="mb-0">
+                      <span>
+                        <Moment format="YYYY/MM/DD">{post.publishedAt}</Moment>
+                      </span>{' '}
+                      | <span>12 min read</span>
+                    </p>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-      ))}
+        ))}
     </Fragment>
   );
 };
